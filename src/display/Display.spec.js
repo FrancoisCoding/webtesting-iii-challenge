67a4a3908fdc45e13bfcf4bd864ead1dc270ab2e
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-19 16:59:25 -0400

    Added Dashboard tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,18 +2,20 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { render, fireEvent } from "@testing-library/react";
 import Display from "./Display";
+// Cleans up unnecessary files
+import "react-testing-library/cleanup-after-each";
 
 describe("<Display />", () => {
   it("Default state set to open and unlocked", () => {
     const { getByText } = render(<Display />);
     // Check if unlocked and open are displayed on screen
-    expect(getByText("Unlocked"));
     expect(getByText("Open"));
+    expect(getByText("Unlocked"));
   });
   it("Accepting props", () => {
     // Check if display component receives props and changes based off received props
     const { getByText } = render(<Display locked={true} closed={true} />);
-    expect(getByText("Locked"));
     expect(getByText("Closed"));
+    expect(getByText("Locked"));
   });
 });
